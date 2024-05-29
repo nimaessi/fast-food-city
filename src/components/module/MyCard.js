006c0ccea9492bc3378c3c23f9591f08138f7c55
nimaessi <nimaesfahani92@gmail.com>
2024-05-29 12:18:24 +0330

    add class cursor pointer to card :wink:

--- src/components/module/MyCard.js
+++ src/components/module/MyCard.js
@@ -6,14 +6,14 @@ import { useRouter } from "next/navigation";
 const MyCard = ({pic, title, id, slug}) => {
 
   const router = useRouter();
-  const handleClick = (id, title) => {
+  const handleClick = (id,slug) => {
     router.push(`ffc-menu/${slug}/${id}`);
   }
   return (
     <Card 
-      className = "w-75 mt-5 mx-auto" 
+      className = "w-75 mt-5 mx-auto cursor-pointer" 
       data-bs-theme = "dark" 
-      onClick={() => {handleClick(id,title)}}>
+      onClick={() => {handleClick(id,slug)}}>
         <Image 
           className = "mx-auto mt-3 img-fluid"
           alt = "product-category" 
